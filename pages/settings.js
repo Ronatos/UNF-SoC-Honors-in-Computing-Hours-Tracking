05f7ca70edf5600c2c0be5814e1c4c40c8847c22
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2022-09-28 15:58:00 -0400

    Another Settings Commit - Trying to figure out css

--- pages/settings.js
+++ pages/settings.js
@@ -2,35 +2,80 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 // import '../styles/setting.css'
+// import "\bootstrap\dist\css\bootstrap.min.css";
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Settings Page</title>
+        <title>ACCOUNT INFO</title>
         <meta name="description" content="The settings page for UNF Leadership" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="fontawesome/css/all.css"></link>
+       
       </Head>
 
+     
+
+
       <main>
+
+      
+        
+         
+        <div className='container'>
+          {/* nav bar account info */}
+          <nav className='top-nav'>
+            <ul style={{ 
+              listStyleType: 'none',
+              margin: 0,
+              padding: 0,
+              overflow: 'hidden',
+             }}>
+
+              <li style={{
+                float: 'left'
+              }}>
+                <a style={{
+
+                }} href='#' target={"_blank"}>Project Settings</a></li>
+              <li><a href='#' target={"_blank"}>Home</a></li>
+              <li ><a href='#' target={"_blank"}>Login</a></li>
+            </ul>
+          </nav>
+
+        
+        
+        
+        </div>
+
   
-      <div>   
+      <div style={{ textAlign: 'center' }}>   
         <h1>ACCOUNT INFO</h1>
         <br></br>
         <h5>GENERAL INFORMATION</h5>
       </div>
 
+      <div className='container'>
+        <div className='leftbox'>
+          <nav> 
+            <a className="tab active"></a>
+            <i className="fa fa-user"></i>
+
+
+          </nav>
+        </div>
+      </div>    
       
 
-      <div>
-      <p style={{ color: 'red' }}>Yo check out</p>
-      </div>
+    
 
       
-
+   
       </main>
     </div>
   )
 }
+
+
 
