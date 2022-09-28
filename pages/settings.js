fb536e30870331e5725a68e61e4d53cba418485d
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2022-09-28 13:44:28 -0400

    Second Commit - Added to Settings Page
    Trying to get the settings page done.

--- pages/settings.js
+++ pages/settings.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-// import '../styles/settings.css'
+// import '../styles/setting.css'
 
 export default function Home() {
   return (
@@ -10,17 +10,27 @@ export default function Home() {
         <title>Settings Page</title>
         <meta name="description" content="The settings page for UNF Leadership" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="fontawesome/css/all.css"></link>
       </Head>
 
       <main>
-        <h1>
-          Welcome to Settings Page!
-        </h1>
+  
+      <div>   
+        <h1>ACCOUNT INFO</h1>
+        <br></br>
+        <h5>GENERAL INFORMATION</h5>
+      </div>
+
+      
+
+      <div>
+      <p style={{ color: 'red' }}>Yo check out</p>
+      </div>
+
+      
 
       </main>
-
     </div>
-
-
   )
 }
+
