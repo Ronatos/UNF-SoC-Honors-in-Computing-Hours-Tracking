a88413828bc81c968f751e1dbfe33c5e1ce32acd
Alexander Perez <n01413497@unf.edu>
2022-11-03 16:26:18 -0400

    started login form submission

---
+++ pages/api/loginForm.js
@@ -0,0 +1,30 @@
+export default function handler(req, res) {
+    // Get data submitted in request's body.
+    const body = req.body
+  
+    // Optional logging to see the responses
+    // in the command line where next.js app is running.
+    console.log('body: ', body)
+  
+    // Guard clause checks for username and password,
+    // and returns early if they are not found
+    if (!body.username || !body.password) {
+        // Sends a HTTP bad request error code
+        return res.status(400).json({ data: 'Username or password not submitted.' })
+    }
+
+    const credentialDB = {
+        username: "alex",
+        password: "test"
+    };
+
+    if (credentialDB.username == body.username && credentialDB.password == body.password) {
+        // 200 OK
+        res.status(200).json({ username: `${body.username}`, password: `${body.password}` })
+    }
+    else {
+        // 400 Bad Request
+        res.status(400).json({ username: `${body.username}`, password: `${body.password}` })
+    }
+}
+  
--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,52 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import unfLogo from '../public/UNF_Logo.gif'
-// import stylesOriginal from '../styles/Home.module.css'
-import styles from '../styles/login.module.css'
 import Link from 'next/link'
 
-let username = "Username"
-let password = "Password"
+import styles from '../styles/login.module.css'
+import unfLogo from '../public/UNF_Logo.gif'
+import RenderResult from 'next/dist/server/render-result'
 
 export default function Login() {
+
+    // Handles the submit event on form submit.
+    const handleSubmit = async (event) => {
+        // Stop the form from submitting and refreshing the page.
+        event.preventDefault()
+
+        // Get data from the form.
+        const data = {
+            username: event.target.username.value,
+            password: event.target.password.value,
+        }
+
+        // Send the data to the server in JSON format.
+        const JSONdata = JSON.stringify(data)
+
+        // API endpoint where we send form data.
+        const endpoint = '/api/loginForm'
+
+        // Form the request for sending data to the server.
+        const options = {
+            // The method is POST because we are sending data.
+            method: 'POST',
+            // Tell the server we're sending JSON.
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // Body of the request is the JSON data we created above.
+            body: JSONdata,
+        }
+
+        // Send the form data to our forms API and get a response.
+        const response = await fetch(endpoint, options)
+
+        // Get the response data from server as JSON.
+        // If server returns the data submitted, that means the form works.
+        const result = await response.json()
+
+        alert(response.status);
+    }
+
     return (
         <div>
             <Head>
@@ -27,10 +65,10 @@ export default function Login() {
                     UNF School of Computing <br></br> Honors in Computing Hours Tracking
                 </h1>
 
-                <form className={styles.description}>
-                    <input type="text" defaultValue={username}/>
+                <form className={styles.description} onSubmit={handleSubmit} method="post">
+                    <input name='username' className={styles.input} type="text" placeholder="Username" required/>
                     <br></br>
-                    <input type="text" defaultValue={password}/>
+                    <input name='password' className={styles.input} type="password" placeholder="Password" required/>
                     <br></br>
                     <input type="submit" value="Log in"/>
                 </form>
