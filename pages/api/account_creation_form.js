55fff6ec2a0febfaccf1d4a5632ad6e6a0a3bf24
Alexander Perez <alexander.l.perez@outlook.com>
2022-12-10 19:19:16 -0500

    fixed account creation and email verification

--- pages/api/account_creation_form.js
+++ pages/api/account_creation_form.js
@@ -14,19 +14,22 @@ export default async function handler(req, res) {
     try {
         const existingUsernames = await dbPool.query("SELECT username FROM accounts WHERE username = ?;", [body.username]);
         if (existingUsernames[0].length > 0) {
-            console.log("400 Bad Request");
+            console.log("400 Bad Request. Username already in use.");
             res.status(400).json({message: "Username already in use."});
+            return;
         }
     }
     catch (e) {
-        console.log("500 Internal Server Error");
+        console.log("500 Internal Server Error. 'SELECT username FROM accounts WHERE username = ?;' failed");
         res.status(500).json({message: e});
+        return;
     }
 
     // Verify the email exists
     if (body.email.match(/^n\d{8}@unf\.edu$/i) == null) {
-        console.log("400 Bad Request");
+        console.log("400 Bad Request. Email doesn't match the regex.");
         res.status(400).json({message: "Must use a valid email."});
+        return;
     }
 
     // Create the account
@@ -45,13 +48,14 @@ export default async function handler(req, res) {
         console.log("The verification code is " + String(array).substring(0, 6) + ". Remove this console log once email verification is complete.");
     }
     catch (e) {
-        console.log("500 Internal Server Error");
+        console.log("500 Internal Server Error. Failed creation of the verification code.");
         res.status(500).json({message: e});
+        return;
     }
 
     // Send an email with a verification code and instructions, enabling the user to submit it on /email_verification to verify their email
     // We will likely want to use the UNF email tenant, which may lead us to the question
     // of why we're not using SSO.
 
-    res.status(200).json({ message: "Account creation successful."})
+    res.status(200).json({ message: "Account creation successful."});
 }
--- pages/api/email_verification_form.js
+++ pages/api/email_verification_form.js
@@ -12,41 +12,44 @@ export default async function handler(req, res) {
     if (body.email.match(/^n\d{8}@unf\.edu$/i) == null) {
         console.log("400 Bad Request");
         res.status(400).json({message: "Must use a valid email."});
+        return;
     }
 
     // Get all data from database that will be needed at once to reduce # of queries
     try {
         // this could return undefined if the user entered the wrong code or an expired one
-        const {account_id} = (await dbPool.query("SELECT account_id FROM email_verification_codes WHERE TIMESTAMPDIFF(MINUTE, creation_time, CURRENT_TIMESTAMP) < 10;", [body.code]))[0][0];
-        console.log(account_id);
+        let {account_id} = (await dbPool.query("SELECT account_id FROM email_verification_codes WHERE TIMESTAMPDIFF(MINUTE, creation_time, CURRENT_TIMESTAMP) < 10;", [body.code]))[0][0];
+        console.log("AccountID = " + account_id);
 
         // if account_id is undefined this will fail
         // could also fail if an account has been deleted without cascading and there are entries for it in the email_verification_codes table
         const {email_address, role} = (await dbPool.query("SELECT email_address, role FROM accounts WHERE account_id = ?;", [account_id]))[0][0];
+        console.log("Email Address = " + email_address);
+        console.log("Role = " + role);
 
         // Verify that the submitted email address matches the email we sent the code that they entered
         if (body.email != email_address) {
-            console.log("400 Bad Request");
-            res.status(400).json({ message: "Invalid verification code."});
+            throw("Invalid or expired verification code.");
         }
+
+        // Change the account from 'email unverified' to either 'active' or 'pending admin approval' depending on role.
+        if (role == 'student') {
+            await dbPool.query('UPDATE accounts SET account_status = "active" WHERE account_id = ?;', [account_id]);
+        }
+        else {
+            await dbPool.query('UPDATE accounts SET account_status = "pending admin approval" WHERE account_id = ?;', [account_id]);
+        }
+
+        // Delete the verification codes for that user from the database. They're not needed anymore, and there could be many.
+        await dbPool.query("DELETE FROM email_verification_codes WHERE account_id = ?;", [account_id]);
     }
     catch (e) {
-        console.log(e);
+        console.log(e); // there are technically 2 errors that could occur here - one should send a 500, the other a 400 (TypeError)
         console.log("400 Bad Request");
         res.status(400).json({ message: "Invalid or expired verification code."});
+        return;
     }
 
     console.log("200 OK");
-
-    // Change the account from 'email unverified' to either 'active' or 'pending admin approval' depending on role.
-    if (role == 'student') {
-        await dbPool.query('UPDATE accounts SET account_status = "active" WHERE account_id = ?;', [account_id]);
-    }
-    else {
-        await dbPool.query('UPDATE accounts SET account_status = "pending admin approval" WHERE account_id = ?;', [account_id]);
-    }
-
-    // Delete the verification codes for that user from the database. They're not needed anymore, and there could be many.
-    await dbPool.query("DELETE FROM email_verification_codes WHERE account_id = ?;", [account_id]);
     res.status(200).json({ message: "Email validation successful."});
 }
