c1969b7a522a91f242428eb133600493b991ed8a
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2023-02-06 14:50:46 -0500

    API Commit - Added code for student Form

--- pages/api/student_form_submit.js
+++ pages/api/student_form_submit.js
@@ -1,10 +1,21 @@
 const { pool: dbPool } = require('@/db/connection');
 
 export default async function handler(req, res) {
+  const body = req.body;
 
-    // Verify the email is valid - not exactly necessary, but might save us a trip to the database
-
-
-  
-}  
-
+  try {
+    const formData = await dbPool.query("INSERT INTO entries (event_name, time_accrued, event_date, faculty_id, latest_comment) VALUES (?, ?, ?, ?, ?);", [
+      body.event,
+      body.hours,
+      body.event_date,
+      body.professor,
+      body.notes
+    ]);
+    console.log("Form data successfully stored in database.");
+    return res.status(200).json({ message: "Form data stored in database successfully." });
+  } catch (e) {
+    console.log("Error storing form data in database.");
+    console.log(e);
+    return res.status(500).json({ message: e });
+  }
+}
