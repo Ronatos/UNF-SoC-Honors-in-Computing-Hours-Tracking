690b9fb4b9f106cc386b0b3d99f45a86826ed7f4
Alexander Perez <n01413497@unf.edu>
2023-02-05 19:35:14 -0500

    added logic to prevent unverified accounts from logging in

--- pages/api/sessions.js
+++ pages/api/sessions.js
@@ -8,17 +8,23 @@ async function createSessionRoute(req, res) {
     if (req.method === "POST") {
         const body = req.body;
 
-        const account_data = (await dbPool.query("SELECT username, password FROM accounts WHERE username = ? AND password = ?;", [body.username, body.password]))[0][0];
+        const account_status = (await dbPool.query("SELECT account_status FROM accounts WHERE username = ? AND password = ?;", [body.username, body.password]))[0][0];
 
-        if (account_data != undefined) {
-            req.session.user = {
-                username: body.username,
-                isAdmin: true
-            };
-            await req.session.save();
-            res.send({ ok: true });
+        if (account_status == undefined) {
+            return res.status(403).json({message: "Invalid username and password combination."});
         }
-        return res.status(403).json({message: "Invalid username and password combination."});
+
+        if (account_status.account_status != 'active') {
+            return res.status(403).json({message: "Email verification required for login."});
+        }
+
+        req.session.user = {
+            username: body.username,
+            isAdmin: true
+        };
+        await req.session.save();
+        res.send({ ok: true });
+    
     }
-    return res.status(404).json({message: "Invalid username and password combination."});
+    return res.status(404).send("");
 }
