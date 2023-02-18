24e9e01cbed5b7b24745f4c7b939977de6e0b426
Alexander Perez <n01413497@unf.edu>
2023-02-18 11:35:25 -0500

    fixed issue where students could submit forms to inactive faculty

--- pages/api/student_form_build.js
+++ pages/api/student_form_build.js
@@ -3,7 +3,7 @@ const { pool: dbPool } = require('@/db/connection');
 export default async function handler(req, res) {
 
     try {
-        const faculty_list = (await dbPool.query('SELECT account_id, first_name, last_name FROM accounts WHERE role = "faculty";', []))[0];
+        const faculty_list = (await dbPool.query("SELECT account_id, first_name, last_name FROM accounts WHERE role = 'faculty' AND account_status = 'active';", []))[0];
         return res.status(200).json({
             faculty_list
         });
