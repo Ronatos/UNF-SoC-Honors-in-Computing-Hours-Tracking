a4cb351b03aae9411fbd58c4165103a081d13520
CrispinWah <111033533+CrispinWah@users.noreply.github.com>
2023-04-07 04:16:27 -0400

    css (bootstrap only loads after refresh need fix)

--- pages/account_requests.js
+++ pages/account_requests.js
@@ -61,7 +61,7 @@ export default function Home() {
             <Head>
                 <title>New Account Requests</title>
                 <meta name="description" content="New Account Requests" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
             </Head>
       
             <header className={styles.header}>
@@ -77,8 +77,8 @@ export default function Home() {
                     <Link href="/home">Home</Link>
                 </div>
 
-                <table className={styles.table} id="tableID">
-                    <thead>
+                <table class="table table-hover table-bordered" className={styles.table} id="tableID">
+                    <thead class="thead-light">
                         <tr>
                             <th className={styles.container}>Username</th>
                             <th className={styles.container}>Name</th>
@@ -116,6 +116,9 @@ export default function Home() {
                         );
                     })}
                 </table>
+                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
             </main>
         </div>
     )
