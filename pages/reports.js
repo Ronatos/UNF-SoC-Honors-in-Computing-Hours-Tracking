2eb9eb59ea63ed3ee7115a573e6c619c593c3322
CrispinWah <111033533+CrispinWah@users.noreply.github.com>
2023-04-11 11:53:17 -0400

    removed scripts need to fix references

--- pages/reports.js
+++ pages/reports.js
@@ -95,7 +95,6 @@ export default function Home() {
             <Head>
                 <title>Reports</title>
                 <meta name="description" content="Reports" />
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
             </Head>
       
             <header className={styles.header}>
@@ -117,8 +116,8 @@ export default function Home() {
                     <input type="text" id="input"  placeholder="Filter" className="search-input" data-table="reports-list" onChange={event => setQuery(event.target.value)}></input>
                 </div>
 
-                <table class="table table-hover table-bordered" id="tableID" className={styles.table}>
-                    <thead class="thead-light">
+                <table className="table table-hover table-bordered" id="tableID">
+                    <thead className="thead-light">
                         <tr>
                             <th onClick={()=>sortingInt("entry_id")}className={styles.container}>Entry ID</th>
                             <th onClick={()=>sortingInt("student_id")}className={styles.container}>Student ID</th>
@@ -178,9 +177,6 @@ export default function Home() {
                         );
                     })}
                 </table>
-                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
             </main>
         </div>
     )
